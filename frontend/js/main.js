6d087149a9066eebaaef354f589f26b1f5656a08
Jonathan ESAÏE <jonathanesaie21@gmail.com>
2026-09-11 14:01:12 +0100

    first commit for my project

---
+++ frontend/js/main.js
@@ -0,0 +1,13 @@
+import {} from "./auth";
+import {} from "./components.js";
+import {} from "./router.js";
+import {} from "./data.js";
+
+import {} from "./pages/dashboard.js";
+import {} from "./pages/livre.js";
+import {} from "./pages/auteur.js";
+import {} from "./pages/adherant.js";
+import {} from "./pages/emprunt.js";
+import {} from "./pages/statistique.js";
+import {} from "./pages/paramettre.js";
+import {} from "./pages/profil.js";
