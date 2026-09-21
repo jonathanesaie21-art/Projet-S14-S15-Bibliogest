eb91d6e9ea7b5a1737c6428be08057876af597f3
Jonathan ESAÏE <jonathanesaie21@gmail.com>
2026-09-21 10:09:28 +0100

    fix:Port

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL =
+  "https://projet-s14-s15-bibliogest.onrender.com/api";
 
 /* ---------------------------- ETAT LOCAL ------------------------------- */
 
